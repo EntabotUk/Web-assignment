61975fec78a30b1be17ad6d36d5b9f7c6e655bba
BortAnovia <33599851+BortAnovia@users.noreply.github.com>
2020-11-26 21:25:43 +0700

    Unecessary changes
    Added comment

--- mainjs.js
+++ mainjs.js
@@ -1,9 +1,9 @@
-/* Set the width of the side navigation to 250px */
+/*  Open category bar to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
   }
   
-  /* Set the width of the side navigation to 0 */
+  /* Close category bar to 0 */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
